23af1bb519fc4c14903eebe06504f2f976f1b6fa
helena-varela <helena.varelakg@gmail.com>
2025-07-17 11:17:22 -0300

    iniciando projeto

--- consultas.py
+++
@@ -1,3 +0,0 @@
-print("name user") 
-qual_consulta = (input("qual consulta voce quer fazer?"))
-print(f"{qual_consulta} voce escolheu essa consultagit  ")
--- pacientes.py
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-nomePaciente=str(input('insira o nome do paciente (nome completo): '))
-print(f'Paciente: {nomePaciente}')
-print (calvos)
-=======
-print("Otario")
->>>>>>> 6e0afd2b28909c6123fd9bcdd0da0dc3da553635
