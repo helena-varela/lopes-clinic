2c1f49097fdd3ef3ee61fea4aa719d25eb833f78
Jessé Ferraz <jessejoaquim3@gmail.com>
2025-07-29 10:07:24 -0300

    def consultas

---
+++ consultas.py
@@ -0,0 +1,8 @@
+nome_paciente = input ("qual o numero da sua consulta?")
+cpf_paciente = input ("qua seu CPF?")
+medico_consulta = input ("qual especialidade voce precisa?")
+
+def consultas(nome_paciente , cpf_paciente , medico_consulta):
+    print("cavgi")
+
+consultas(nome_paciente , cpf_paciente , medico_consulta)
